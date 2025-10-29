7a035ead44af16084a6e095925e59a0b2f716f04
Matheus Vicente <vicentematheus1507@gmail.com>
2025-10-28 22:17:31 -0300

    add validation for password length in setSenha method of Usuario class

--- Parte 3/Usuario.js
+++ Parte 3/Usuario.js
@@ -3,10 +3,17 @@ class Usuario {
     #email
 
     setSenha(novaSenha){
-
+        if (novaSenha.lenght >= 6){
+            console.log(`Senha autenticada com sucesso!`)
+        }else{
+            console.log(`A senha deve ter 6 ou mais caracteres!`)
+        }
     }
 
     autenticar(senhaInformada){
         
     }
 }
+
+const usuario1 = new Usuario();
+usuario1.setSenha("1234567");
