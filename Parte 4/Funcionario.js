e774c52be05a3d2ce5ed27a9bec01ba9cfa83e6b
Matheus Vicente <vicentematheus1507@gmail.com>
2025-11-03 21:50:32 -0300

    feat: implement salary calculation in FuncionarioHorista class

--- Parte 4/Funcionario.js
+++ Parte 4/Funcionario.js
@@ -14,6 +14,6 @@ class FuncionarioHorista extends Funcionario {
         this.valorHora = valorHora;
     }
     calcularSalario(){
-        
+        return this.horasTrabalhadas * this.valorHora
     }
 }
