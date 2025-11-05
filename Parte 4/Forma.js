4719452cb11c22c34be5708bc06489b07dcc20ff
Matheus Vicente <vicentematheus1507@gmail.com>
2025-11-05 18:13:17 -0300

    fix: correct constructors and area calculations for Quadrado and Retangulo subclasses

--- Parte 4/Forma.js
+++ Parte 4/Forma.js
@@ -6,17 +6,25 @@ class Forma {
 }
 
 class Quadrado extends Forma {
-    constructor(base, altura){
-        this.base = base;
-        this.altura = altura;
+    constructor(lado){
+        super()
+        this.lado = lado;
     }
     calcularArea(){
-        const area = this.base * this.altura
+        const area = this.lado * this.lado
     }
 }
 
 class Retangulo extends Forma {
+    constructor(lado){
+        super()
+        this.lado = lado;
+    }
+    calcularArea(){
+        const area = this.lado * this.lado
+    }
     constructor(base, altura){
+        super()
         this.base = base;
         this.altura = altura;
     }
